229bfe67656a842d59bc979985f203b60a996f33
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-07-27 16:34:29 -0400

    Frist Commit

---
+++ autocp2k/__init__.py
@@ -0,0 +1,30 @@
+"""
+This is the autogamess module, a python module designed to automate
+optimization, hessian, raman, and VSCF input generation and output
+data collection.
+
+Contains the following functions:
+                new_project
+                opt2hes
+                sort_logs
+                hes2raman
+                get_data
+                input_builder
+
+
+Author: Brian C. Ferrari
+"""
+    #Configuration (holds Globals)
+from .config        import *
+
+    #Functions
+from .new_project       import new_project
+from .opt2hes           import opt2hes
+from .sort_logs         import sort_logs
+from .hes2raman         import hes2raman
+from .input_builder     import input_builder
+from .fill_spreadsheets import fill_spreadsheets
+from .get_data          import get_data
+
+    #Multi-function Modules
+from .data_finder       import *
